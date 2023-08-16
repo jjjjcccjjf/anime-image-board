cd196638448a252e8472ab16cb740294b480f05b
Ro11ingButler <rollingkai1214@gmail.com>
2023-08-16 18:15:53 +0800

    Fix cookie feature

--- app/components/Cookie.tsx
+++ app/components/Cookie.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Abel } from "next/font/google";
+import Cookies from "js-cookie";
 
 const montserrat = Abel({
   weight: "400",
@@ -7,24 +10,76 @@ const montserrat = Abel({
   subsets: ["latin"],
 });
 
+const generateRandomString = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+};
+
 export default function Cookie() {
+  const [isAcceptingCookies, setIsAcceptingCookies] = useState(() => {
+    const isAcceptingCookies = localStorage.getItem("isAcceptingCookies");
+
+    switch (isAcceptingCookies) {
+      case "true":
+        return true;
+      case "false":
+        return false;
+      case null:
+      default:
+        return null;
+    }
+  });
+
+  useEffect(() => {
+    if (isAcceptingCookies) {
+      const existingCookie = Cookies.get("token");
+      if (!existingCookie) {
+        const randomString = generateRandomString(30);
+        Cookies.set("token", randomString, { expires: 30 }); // Expires in 30 days
+      }
+    }
+  }, [isAcceptingCookies]);
+
+  const handleCookieButton = (wantCookies: boolean) => {
+    setIsAcceptingCookies(wantCookies);
+    localStorage.setItem("isAcceptingCookies", wantCookies.toString());
+  };
+
   return (
-    <section
-      className={`fixed bottom-5 flex w-full items-center justify-center ${montserrat.className}`}
-    >
-      <div className=" h-32 w-1/2 bg-green-200 p-4 uppercase">
-        <h2 className="text-lg">Cookie Notice</h2>
-        <p className="text-base">
-          We use cookies to enhance your browsing experience. By clicking
-          &quot;Accept,&quot; you consent to the use of all cookies.
-        </p>
-        <div className="flex items-center gap-4">
-          <button className="bg-red-400 p-2 text-lg uppercase">
-            Reject all cookies
-          </button>
-          <button className="bg-green-400 p-4 text-lg uppercase">Accept</button>
-        </div>
-      </div>
-    </section>
+    <>
+      {isAcceptingCookies === null && (
+        <section
+          className={`fixed bottom-5 flex w-full items-center justify-center ${montserrat.className}`}
+        >
+          <div className=" flex h-36 w-1/2 flex-col gap-1 bg-green-200 p-4 uppercase">
+            <h2 className="">Cookie Notice</h2>
+            <p className="text-base">
+              We use cookies to enhance your browsing experience. By clicking
+              &quot;Accept,&quot; you consent to the use of all cookies.
+            </p>
+            <div className="flex items-center gap-2">
+              <button
+                className="bg-red-400 px-2 py-1 uppercase"
+                onClick={() => handleCookieButton(false)}
+              >
+                Reject all cookies
+              </button>
+              <button
+                className="bg-green-400 px-2 py-1 uppercase"
+                onClick={() => handleCookieButton(true)}
+              >
+                Accept all cookies
+              </button>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
--- app/components/GridItemTest.tsx
+++ app/components/GridItemTest.tsx
@@ -2,30 +2,36 @@
 
 import React, { useEffect, useState } from "react";
 import GridItem from "./GridItem";
+import Cookies from "js-cookie";
 const { NekosAPI } = require("nekosapi");
 
 const nekos = new NekosAPI();
 
 export default function GridItemTest() {
   const [images, setImages] = useState([]);
+  const [token] = useState(() => Cookies.get("token") ?? null);
 
   useEffect(() => {
-    fetch(`https://api.nekosapi.com/v2/images?page[limit]=25&page[offset]=0&filter[ageRating.iexact]=sfw`)
+    let apiUrl =
+      "https://api.nekosapi.com/v2/images?page[limit]=25&page[offset]=0&filter[ageRating.iexact]=sfw";
+
+    apiUrl = apiUrl + (token ? "&token=" + token : "");
+
+    fetch(apiUrl)
       .then((res) => res.json())
       .then((res) => {
         console.log(res.data);
-        
-        setImages(res.data)
+
+        setImages(res.data);
       });
   }, []);
 
   return (
     <>
-    
-        {images && images.map((image, index) => {
-            return <GridItem key={index} src={image.attributes.file} />
+      {images &&
+        images.map((image, index) => {
+          return <GridItem key={index} src={image.attributes.file} />;
         })}
-       
     </>
   );
 }
